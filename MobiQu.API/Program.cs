ae7e93cf864a202fa1e87b83ee28ad93e8400701
Atakan Acar <atakan.acar@farklabs.com>
2021-12-24 12:33:24 +0300

    push

--- MobiQu.API/Program.cs
+++ MobiQu.API/Program.cs
@@ -20,8 +20,6 @@ namespace MobiQu.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://0.0.0.0:5000;https://0.0.0.0:5001");
-                    webBuilder.UseKestrel();
                     webBuilder.UseStartup<Startup>();
                 });
     }
