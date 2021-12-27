124d37a553e3b1b9d9ee6267f49f0d0ede738e6e
Atakan Acar <atakan.acar@farklabs.com>
2021-12-27 12:30:23 +0300

    git pull

--- MobiQu.API/Startup.cs
+++ MobiQu.API/Startup.cs
@@ -81,7 +81,7 @@ namespace MobiQu.API
             
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this nmethod to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
